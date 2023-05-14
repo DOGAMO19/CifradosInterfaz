939fcadce48f7838e0b64b637f5cbdc2ce1c2d9c
DOGAMO19 <danidogm19@gmail.com>
2023-05-13 21:10:06 -0600

    Actualizacion Descifrado Doble

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -61,3 +61,34 @@ export const InputFormDescifrar = ({title,subtitle,sitioDescifrar,setSitioDescif
     </div>
   )
 }
+
+
+export const InputFormDoble = ({title,subtitle,sitioCifrarDos,setSitioCifrarDos,resultadoCifradoDos}) => {
+
+  return (
+    <div >
+      <form  className=' flex flex-col justify-center items-center'>
+      <label htmlFor="inputLink" className=" text-xl mb-5">{title}</label>
+      <input 
+        type="text"
+        name="inputLink"
+        id='inputLink'
+        placeholder='Link'
+        className=" placeholder:italic placeholder:text-gray-700 block bg-white w-3/4 border border-gray-600 rounded-md py-1 pl-3 pr-3 shadow-md focus:outline-none focus:border-sky-500 sm:text-sm" 
+        value={sitioCifrarDos}
+        onChange={ (event) => setSitioCifrarDos(event.target.value) }
+        />
+      <p className="mt-5 text-lg">{subtitle}</p>
+      <textarea
+        name="resultadoDescifrado"  
+        cols="30" 
+        rows="10" 
+        placeholder="Resultado" 
+        defaultValue={resultadoCifradoDos}
+        className=" resize-none mt-5 placeholder:italic placeholder:text-gray-700 block bg-white w-3/4 border border-gray-600 rounded-md py-1 pl-3 pr-3 shadow-md focus:outline-none focus:border-sky-500 sm:text-sm" />
+      
+      </form>
+
+    </div>
+  )
+}
--- src/pages/CifradoAes.jsx
+++ src/pages/CifradoAes.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import CryptoJS from "crypto-js"
-import { InputForm, InputFormDescifrar } from "../components/InputForm"
+import { InputForm, InputFormDescifrar, InputFormDoble } from "../components/InputForm"
 import { Header } from "../layouts/Header"
 
 
@@ -8,7 +8,9 @@ export const CifradoAes = () => {
 
 
   const [sitio, setSitio] = useState("")
+  const [sitioCifrarDos, setSitioCifrarDos] = useState("")
   const [resultadoCifrado, setResultadoCifrado] = useState("")
+  const [resultadoCifradoDos, setResultadoCifradoDos] = useState("")
   const [sitioDescifrar, setSitioDescifrar] = useState("")
   const [resultadoDescifrado, setResultadoDescifrado] = useState("")
 
@@ -20,13 +22,38 @@ export const CifradoAes = () => {
   const descifrarAes = (texto) => {
     let cifradoBytes = CryptoJS.AES.decrypt(texto,'@criptografia')
     let textoDescifrado = cifradoBytes.toString(CryptoJS.enc.Utf8)
-    return setResultadoDescifrado(textoDescifrado)
+    let textoDescifradoDoble = window.atob(textoDescifrado)
+    return setResultadoDescifrado(textoDescifradoDoble)
+  }
+
+  const cifrarBase = (texto) =>{
+    let textoCifrado = btoa(texto)
+    return setResultadoCifradoDos(textoCifrado)
   }
 
   return (
       <div className="bg-gray-300 min-h-screen">
         <Header />
         <div className="container shadow-md rounded-md bg-gray-200 w-3/4 mt-5 h-full pt-6 pb-6">
+          {/* Form cifrado Base64 */}
+          <InputFormDoble 
+          title = "Link a cifar en base64 - Primer Cifrado"
+          subtitle = "Link cifrado a base64"
+          sitioCifrarDos = { sitioCifrarDos }
+          setSitioCifrarDos = { setSitioCifrarDos }
+          resultadoCifradoDos={ resultadoCifradoDos }
+          setResultadoCifradoDos = { setResultadoCifradoDos }
+          />
+          <div className="flex flex-col justify-center items-center">
+            <button
+            type="submit"
+            onClick={() => cifrarBase(sitioCifrarDos)}
+            className=" mb-5 bg-cyan-500 hover:bg-cyan-700 cursor-pointer text-white font-semibold md:w-1/4 w-1/2 p-3 mt-5 uppercase text-lg"  
+            >
+            Cifrar a Base64
+            </button>
+          </div>
+          {/* Form cifrado AES */}
           <InputForm 
           title="Link a cifrar en formato AES" 
           subtitle="Resultado cifrado en formato AES"
@@ -44,7 +71,7 @@ export const CifradoAes = () => {
               Cifrar a formato AES
             </button>
           </div>
-  
+          {/* Form descifrado de AES  */}
           <InputFormDescifrar
           title="Link a descifrar en formato AES" 
           subtitle="Resultado descifrado en formato AES"
@@ -59,7 +86,7 @@ export const CifradoAes = () => {
               onClick={() => descifrarAes(sitioDescifrar)}
               className="  bg-cyan-500 hover:bg-cyan-700 cursor-pointer text-white font-semibold md:w-1/4 w-1/2 p-3 mt-5 uppercase text-lg"  
             >
-              Descifrar de formato AES
+              Descifrar de formato AES y Base64
             </button>
           </div>
         </div>
